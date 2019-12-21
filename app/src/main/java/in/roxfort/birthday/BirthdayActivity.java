3ea3fc5ca505a6d74074d6041692071af6e20d2d
rajnish725 <diction.rajnish@gmail.com>
2019-12-21 12:02:19 +0530

    test admobs

--- app/src/main/java/in/roxfort/birthday/BirthdayActivity.java
+++ app/src/main/java/in/roxfort/birthday/BirthdayActivity.java
@@ -6,7 +6,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -23,17 +25,39 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.doubleclick.PublisherAdRequest;
 import com.google.android.gms.ads.doubleclick.PublisherAdView;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.android.gms.ads.reward.RewardItem;
+import com.google.android.gms.ads.reward.RewardedVideoAd;
+import com.google.android.gms.ads.reward.RewardedVideoAdListener;
 
 public class BirthdayActivity extends AppCompatActivity {
+    private final String TAG = BirthdayActivity.class.getSimpleName();
     private TextView txt_birthday, txt_birthday_name;
-    Dialog dialog;
-    Button btn_stop;
-    Context mContext;
-    boolean isYes = true;
-    EditText edittext;
     private PublisherAdView mPublisherAdView;
+    Context mContext = null;
+    boolean isYes = true;
+    Dialog dialog = null;
+    EditText edittext;
+    private String name;
+    Button btn_stop;
+
+
+    /*ads declaration*/
+
+    private static final String AD_UNIT_ID = "ca-app-pub-6805683297486423/3691042196";
+
+    private InterstitialAd interstitialAd;
+    private CountDownTimer countDownTimer;
+    private boolean gameIsInProgress;
+    AdRequest requestAd;
+    private long timerMilliseconds;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,50 +67,88 @@ public class BirthdayActivity extends AppCompatActivity {
         hideSoftKeyboard(mContext);
         getWindow().setSoftInputMode(
                 WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
-//        getSupportActionBar().hide();
         if (isYes) {
             dialogName();
             isYes = false;
         }
-      /*  MobileAds.initialize(this,
-                getString(R.string.admob_app_id));*/
 
+        // TODO: 20/12/19
+        //todo Initialize the Mobile Ads SDK.
+        requestAd = new AdRequest.Builder().build();
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
         mPublisherAdView = findViewById(R.id.adView);
         PublisherAdRequest adRequest = new PublisherAdRequest.Builder().build();
         // Display Banner ad
         mPublisherAdView.loadAd(adRequest);
+//        loadRewardedVideoAd();
 
         edittext = findViewById(R.id.edittext);
         txt_birthday = findViewById(R.id.txt_birthday);
         txt_birthday_name = findViewById(R.id.txt_birthday_name);
         btn_stop = findViewById(R.id.btn_stop);
 
-
-        txt_birthday.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
         txt_birthday_name.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), BackgroundSoundService.class);
                 stopService(intent);
-
                 return true;
             }
         });
 
 
-        btn_stop.setOnClickListener(new View.OnClickListener() {
+        // Create the InterstitialAd and set the adUnitId.
+        interstitialAd = new InterstitialAd(this);
+        // Defined in res/values/strings.xml
+        interstitialAd.setAdUnitId(AD_UNIT_ID);
+
+
+        interstitialAd.setAdListener(new AdListener() {
             @Override
-            public void onClick(View v) {
+            public void onAdLoaded() {
+                showInterstitial();
+            }
 
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
 
+                txt_birthday_name.setVisibility(View.VISIBLE);
+                txt_birthday_name.setText(name);
+                Intent intent = new Intent(getApplicationContext(), BackgroundSoundService.class);
+                startService(intent);
+                RunAnimation();
+
+            }
+
+            @Override
+            public void onAdClosed() {
+//                startGame();
+
+                txt_birthday_name.setVisibility(View.VISIBLE);
+                txt_birthday_name.setText(name);
+                Intent intent = new Intent(getApplicationContext(), BackgroundSoundService.class);
+                startService(intent);
+                RunAnimation();
             }
         });
 
+    }
+
+    private void showInterstitial() {
+        try {
+            Log.e(TAG, "inside   try");
+            if (interstitialAd != null) {
+                interstitialAd.loadAd(requestAd);
+                interstitialAd.show();
+            }
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage());
+
+        }
 
     }
 
@@ -95,7 +157,6 @@ public class BirthdayActivity extends AppCompatActivity {
         getWindow().setSoftInputMode(
                 WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
         dialog = new Dialog(mContext);
-//         dialog.set(android.app.DialogFragment.STYLE_NO_TITLE, R.style.DialogStyle);
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
         LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View view = inflater.inflate(R.layout.dialog_add_name, null, false);
@@ -109,7 +170,6 @@ public class BirthdayActivity extends AppCompatActivity {
         ((Activity) mContext).getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE | WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
         dialog.setContentView(view);
         final Window window = dialog.getWindow();
-//        window.setLayout(600, 250);
         window.setLayout(WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT);
 
         window.setGravity(Gravity.CENTER);
@@ -123,7 +183,7 @@ public class BirthdayActivity extends AppCompatActivity {
         btn_continue.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String name = edt_name.getText().toString().trim();
+                name = edt_name.getText().toString().trim();
                 getWindow().setSoftInputMode(
                         WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
                 if (TextUtils.isEmpty(name)) {
@@ -133,23 +193,16 @@ public class BirthdayActivity extends AppCompatActivity {
                     txt_error.setVisibility(View.VISIBLE);
 
                 } else {
+                    name = edt_name.getText().toString();
+                    showInterstitial();
 
-                    txt_birthday_name.setVisibility(View.VISIBLE);
-                    txt_birthday_name.setText(edt_name.getText().toString());
-                    Intent intent = new Intent(getApplicationContext(), BackgroundSoundService.class);
-                    startService(intent);
-                    RunAnimation();
+
                     dialog.dismiss();
-
-
                 }
                 getWindow().setSoftInputMode(
                         WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
-
             }
         });
-
-
     }
 
     private void RunAnimation() {
@@ -159,7 +212,6 @@ public class BirthdayActivity extends AppCompatActivity {
         txt_birthday_name.startAnimation(a);
     }
 
-
     public void hideSoftKeyboard(Context mContext) {
         InputMethodManager imm = (InputMethodManager)
                 mContext.getSystemService(Context.INPUT_METHOD_SERVICE);
@@ -168,4 +220,6 @@ public class BirthdayActivity extends AppCompatActivity {
 
 
     }
+
+
 }
